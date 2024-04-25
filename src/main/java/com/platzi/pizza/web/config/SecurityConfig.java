7e770370279916e7afedcd22acfdef1f9746f8c7
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-25 17:42:10 -0500

    deshabilitando csrf

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.platzi.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,11 +15,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) ->
                         authorize
                                 .anyRequest()
                                 .authenticated()
-                ).httpBasic(withDefaults());
+                )
+                .httpBasic(withDefaults());
         return http.build();
     }
 
