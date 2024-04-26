aa168fe255b018d47478042a3c2ab2be99a11281
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-25 20:12:52 -0500

    habilitando cors con http

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests((authorize) ->
                         authorize
                                 .anyRequest()
